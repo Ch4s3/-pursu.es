cdc166e7ad55c1d2446a54fbcf7f3d9c0e7b8292
Chase <chase.gilliam@gmail.com>
2016-11-22 01:02:39 -0500

    deploy strategy and removed elm debug

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ source 'https://rubygems.org'
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
 gem "middleman-blog", "~> 4.0"
+gem "mime-types"
+gem "middleman-s3_sync"
--- config.rb
+++ config.rb
@@ -44,3 +44,21 @@ end
 # Build-specific configuration
 configure :build do
 end
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'chase.pursu.es' # The name of the S3 bucket you are targeting. This is globally unique.
+  s3_sync.region                     = 'us-east-1'     # The AWS region for your bucket.
+  # s3_sync.aws_access_key_id          = 'AWS KEY ID'
+  # s3_sync.aws_secret_access_key      = 'AWS SECRET KEY'
+  s3_sync.delete                     = false # We delete stray files by default.
+  s3_sync.after_build                = false # We do not chain after the build step by default.
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+  s3_sync.index_document             = 'index.html'
+  s3_sync.error_document             = '404.html'
+end
