b6a5cc568a803a3bc03ff47c0763412084c59e9e
Chase <chase.gilliam@gmail.com>
2017-04-09 23:29:18 -0400

    bump webpack, rename some vars

--- sync.rb
+++ sync.rb
@@ -9,12 +9,13 @@ module Middleman
       env_vars = YAML.load(File.open("secrets.yml").read)
       Algolia.init application_id: env_vars["application_id"],
                    api_key: env_vars["api_key"]
-      @index = Algolia::Index.new("posts")
-      @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+      @algolia_index = Algolia::Index.new("posts")
+      @redcarpet = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
       super
     end
 
     def after_build(builder)
+      binding.pry
       Dir.glob("source/*.markdown") do |markdown_file|
         markdown_post = File.open(markdown_file).read
         article_meta_data = YAML.load(markdown_post.lines[1..3].join("\n"))
@@ -23,11 +24,14 @@ module Middleman
         date = article_meta_data["date"]
         unix_time = Date.parse(date).to_time.to_i
         markdown_body = markdown_post.lines[5..-1].join("\n")
-        body_html = @markdown.render(markdown_body).tr("\n\n", " ")
+        body_html = @redcarpet.render(markdown_body).tr("\n\n", " ")
         text = Nokogiri::HTML(body_html).text.squeeze(" ")
-        post = {objectID: Base64.encode64(title), title: title, tags: tags, text: text, date: unix_time}
+        post =
+          { objectID: Base64.encode64(title),
+            title: title, tags: tags,
+            text: text, date: unix_time }
         begin
-          @index.save_object(post)
+          @algolia_index.save_object(post)
         rescue => e
           puts "error: #{e}"
         end
