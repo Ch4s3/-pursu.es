0ca1f78dc2990295b7afc131ae9cdc6a1d5a6dbe
Chase <chase.gilliam@gmail.com>
2018-01-29 16:45:02 -0500

    updates, rm circle, trying h2

--- Gemfile
+++ Gemfile
@@ -1,16 +1,14 @@
-# If you do not have OpenSSL installed, change
-# the following line to use "http://"
-source "https://rubygems.org"
+source 'https://rubygems.org'
+# Algolia gem for sync
+gem 'algoliasearch'
 # Middleman Gems
-gem "middleman", "~>4.2.1"
-gem "middleman-blog", "~> 4.0"
-gem "middleman-minify-html"
-gem "middleman-syntax"
-gem "middleman-s3_sync"
-gem "mime-types"
-
-gem "algoliasearch"
-gem "redcarpet"
-
-gem "middleman-livereload"
-gem "pry"
+gem 'middleman', '~>4.2.1'
+gem 'middleman-blog', '~> 4.0'
+gem 'middleman-livereload'
+gem 'middleman-minify-html'
+gem 'middleman-s3_sync'
+gem 'middleman-syntax'
+# Utilities
+gem 'mime-types'
+gem 'pry'
+gem 'redcarpet'
--- sync.rb
+++ sync.rb
@@ -1,31 +1,33 @@
-require "pry"
-require "algoliasearch"
+# require 'pry'
+require 'algoliasearch'
 require 'redcarpet'
 require 'redcarpet'
 
 module Middleman
+  # utility class for syncing articles with algolia
   class AlgoliaSync < ::Middleman::Extension
     def initialize(app, options_hash={}, &block)
-      env_vars = YAML.load(File.open("secrets.yml").read)
-      Algolia.init application_id: env_vars["application_id"],
-                   api_key: env_vars["api_key"]
-      @algolia_index = Algolia::Index.new("posts")
+      env_vars = YAML.load(File.open('secrets.yml').read)
+      Algolia.init application_id: env_vars['application_id'],
+                   api_key: env_vars['api_key']
+      @algolia_index = Algolia::Index.new('posts')
       @redcarpet = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
       super
     end
 
     def after_build(builder)
-      Dir.glob("source/articles/*/*.markdown") do |markdown_file|
+      Dir.glob('source/articles/*/*.markdown') do |markdown_file|
         markdown_post = File.open(markdown_file).read
-        article_meta_data = YAML.load(markdown_post.lines[1..3].join("\n"))
-        title = article_meta_data["title"]
-        article_link = "/" + title.downcase.tr(" ","-") + ".html"
-        tags = article_meta_data["tags"].split(",")
-        date = article_meta_data["date"]
+        article_meta_data = YAML.safe_load(markdown_post.lines[1..3].join("\n"))
+        title = article_meta_data['title']
+        sanatized_title = title.downcase.tr(' ', '-').tr('/', '-')
+        article_link = "/#{sanatized_title}.html"
+        tags = article_meta_data['tags'].split(',')
+        date = article_meta_data['date']
         unix_time = Date.parse(date).to_time.to_i
         markdown_body = markdown_post.lines[5..-1].join("\n")
-        body_html = @redcarpet.render(markdown_body).tr("\n\n", " ")
-        text = Nokogiri::HTML(body_html).text.squeeze(" ")
+        body_html = @redcarpet.render(markdown_body).tr("\n\n", ' ')
+        text = Nokogiri::HTML(body_html).text.squeeze(' ')
         post =
           { objectID: Base64.encode64(title),
             title: title, article_link: article_link,
